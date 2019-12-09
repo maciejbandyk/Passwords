805b4fd65bde91b2f59e69ec1fbc3f7faf6efadf
TomaszOrpik <47402951+TOrpik@users.noreply.github.com>
2019-12-09 03:27:15 +0100

    azureConMethod
    Added method in class library which will establish azure - app connection

---
+++ PassManager/PasswordManager/PassManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace PassManager
+{
+    public class PassManger
+    {
+        //method receives sql commands and send them to Azure
+        public void AzureConnect(string[] commands)
+        {
+            try
+            {
+                SqlConnectionStringBuilder build = new SqlConnectionStringBuilder();
+                build.DataSource = "password.database.windows.net";
+                build.UserID = "Admin1";
+                build.Password = "Pass123!";
+                build.InitialCatalog = "PassManager";
+
+                using (SqlConnection connection = new SqlConnection(build.ConnectionString))
+                {
+                    //Console.WriteLine("Connection estabilished");
+
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(commands[0]);  //send sql command from commands arr to azure (every line is new append)
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        //command received
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                //Console.WriteLine(reader.GetString(1) + "|" + reader.GetString(2)); //print results
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException sE)
+            {
+                Console.WriteLine(sE.ToString());
+            }
+        }
+    }
+}
